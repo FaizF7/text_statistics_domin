dbeab9244bed9da86fdde437fefee35a4da913a0
Faiz Fazaluddin <faiz.mf07@gmail.com>
2024-01-05 02:04:36 +0000

    able to update some num words and avg length on input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [text, setText] = useState("")
+  const [wordsList, setWordsList] = useState([])
+  const [stats, setStats] = useState({
+    numberWords:"0",
+    avgLength:0,
+    mostFreqWord:"",
+    countWordLengths:{}
+  })
+
+  useEffect(()=>{
+
+    calculateStats()
+
+  },[wordsList])
+
+  const onInput = (event)=>{
+    setText(event.target.value)
+    setWordsList(event.target.value.split(" ").filter(t => t!=""))
+  }
+
+  const avgWordLength = ()=>{
+    return wordsList.reduce((avg, word)=> avg+=word.length/wordsList.length,0)
+  }
+
+  const calculateStats = ()=>{
+    if (text.length==0){
+      setStats(
+        {
+          numberWords:"0",
+          avgLength:0,
+          mostFreqWord:"",
+          countWordLengths:{}
+        }
+      )
+    }
+    else{
+    const updatedStats = {...stats}
+    updatedStats.numberWords = wordsList.length
+    updatedStats.avgLength = avgWordLength()
+
+
+    setStats(updatedStats)
+    }
+    
+  }
+
   return (
     <>
       <h1>Text Statistics</h1>
+
+      <input type='text' onChange={onInput}/>
+
+      <p>Total number of words: {stats.numberWords}</p>
+      <p>Average word length:{stats.avgLength}</p>
+      <p>Most frequently occurring word length:</p>
+      <p>A list of the number of words of each length:</p>
+
     
     </>
   );
